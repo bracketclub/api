f53b30a1da911cb7603546bcab0ba0b4c616ab2d
Luke Karrys <lukekarrys@gmail.com>
2015-03-07 16:58:40 -0700

    dont start masters with empty json

--- test/json-data.js
+++ test/json-data.js
@@ -7,12 +7,12 @@ var assert = require('assert');
 describe('Data', function () {
     it('Has masters', function () {
         var data = liveData({year: '2013', sport: 'ncaa-mens-basketball'});
-        assert.equal(64, data.masters.length);
+        assert.equal(63, data.masters.length);
     });
 
     it('Has masters', function () {
         var data = liveData({year: '2012', sport: 'ncaa-mens-basketball'});
-        assert.equal(64, data.masters.length);
+        assert.equal(63, data.masters.length);
     });
 
     it('Has entries', function () {
