407516bd17a7f3b50898e59f0c4d969427c9de04
Luke Karrys <lukekarrys@gmail.com>
2015-03-10 17:10:51 -0700

    tmux bash script

---
+++ tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SESSIONNAME="tyb-api"
+
+if [ $1 === "start" ] ; then
+    tmux new -s $SESSIONNAME -n api -d
+    tmux send-keys -t $SESSIONNAME "cd $HOME/api && npm start" C-m
+fi
+
+if [ $1 === "stop" ] ; then
+    tmux kill-session -t $SESSIONNAME
+fi
