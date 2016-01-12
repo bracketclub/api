78af8f7ae8d271bfaaa5e2783cfb2920975456ec
Luke Karrys <lukekarrys@gmail.com>
2016-01-11 23:18:50 -0700

    nested queries; start masters data with empty bracket always

--- services/api/controllers/entries.js
+++ services/api/controllers/entries.js
@@ -1,45 +1,35 @@
 'use strict';
 
 const Joi = require('joi');
-const _ = require('lodash');
 
 const sseHandler = require('../lib/sseHandler');
 const utils = require('../lib/reply');
-const where = require('../lib/where');
+
+const entriesQuery = (where) => `SELECT
+  e.bracket, e.data_id, e.created,
+  row_to_json(u) as user
+  FROM entries e, users u
+  WHERE ${where ? `${where} AND` : ''} e.user_id = u.user_id
+  GROUP BY u.*, e.data_id;`;
 
 module.exports = {
   all: {
     description: 'All entries',
     tags: ['api', 'entries'],
     handler: (request, reply) => {
-      const clauses = [];
+      const year = request.query.year;
+      const response = (err, res) => reply(err, utils.all(res));
 
-      if (!_.isEmpty(request.query)) {
-        const year = request.query.year;
-        const user = request.query.user;
-
-        if (year) {
-          clauses.push({
-            text: 'extract(YEAR from created) = $',
-            value: year
-          });
-        }
-
-        if (user) {
-          clauses.push({
-            text: 'user_id = $',
-            value: user
-          });
-        }
+      if (year) {
+        request.pg.client.query(entriesQuery('extract(YEAR from created) = $1'), [year], response);
       }
-
-      const query = where(clauses);
-      request.pg.client.query(`SELECT * FROM entries ${query.text};`, query.values, (err, res) => reply(err, utils.all(res)));
+      else {
+        request.pg.client.query(entriesQuery(), response);
+      }
     },
     validate: {
       query: {
-        year: Joi.string().regex(/^20\d\d$/),
-        user: Joi.string().regex(/^[\d]+$/)
+        year: Joi.string().regex(/^20\d\d$/)
       }
     }
   },
@@ -47,7 +37,7 @@ module.exports = {
     description: 'Get entry by id',
     tags: ['api', 'entries'],
     handler: (request, reply) => {
-      request.pg.client.query('SELECT * FROM entries WHERE data_id = $1;', [request.params.id], (err, res) => {
+      request.pg.client.query(entriesQuery('data_id = $1'), [request.params.id], (err, res) => {
         reply(err, utils.get(res));
       });
     },
