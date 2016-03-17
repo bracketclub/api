49942ffb98e7bb0c6508b4e9df70ced43b51f992
Luke Karrys <lukekarrys@gmail.com>
2016-03-17 12:20:26 -0700

    access control on event streams

--- services/api/lib/sseHandler.js
+++ services/api/lib/sseHandler.js
@@ -8,6 +8,9 @@ module.exports = function sseHandler(channels) {
     request.raw.req.on('close', () => channels.remove(channel.id));
     reply(channel.channel).code(SUCCESS)
       .type('text/event-stream')
+      .header('Access-Control-Allow-Credentials', true)
+      .header('Access-Control-Allow-Origin', '*')
+      .header('Access-Control-Expose-Headers', 'WWW-Authenticate,Server-Authorization')
       .header('Connection', 'keep-alive')
       .header('Cache-Control', 'no-cache')
       .header('Content-Encoding', 'identity');
