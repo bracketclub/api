f67c5dfbc1e1fa7ead54b238fc2ba354782d103f
Luke Karrys <lukekarrys@gmail.com>
2016-12-08 19:32:03 -0700

    sse

--- services/routes/index.js
+++ services/routes/index.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const PGPubsub = require('pg-pubsub');
-
 const packageInfo = require('../../package');
-const Channels = require('./lib/channels');
-const postgresOptions = require('../../lib/postgres');
 
 const users = require('./controllers/users');
 const entries = require('./controllers/entries');
@@ -12,10 +8,6 @@ const masters = require('./controllers/masters');
 
 exports.register = (plugin, options, done) => {
   plugin.bind({config: options.config});
-
-  const sub = new PGPubsub(postgresOptions.connectionString);
-  const entryChannel = new Channels();
-  const masterChannel = new Channels();
 
   // Users
   plugin.route({method: 'GET', path: '/users/{id}', config: users.get});
@@ -25,22 +17,13 @@ exports.register = (plugin, options, done) => {
   plugin.route({method: 'GET', path: '/entries/{sport}-{year}', config: entries.all});
   plugin.route({method: 'GET', path: '/entries/{id}', config: entries.get});
 
-  // Entries events
-  plugin.route({method: 'GET', path: '/entries/events', config: entries.events(entryChannel)});
-  sub.addChannel('entries', (event) => entryChannel.write({event: `entries-${event}`}));
-  sub.addChannel('users', (id) => entryChannel.write({event: 'users', data: {id}}));
-
   // Masters
   plugin.route({method: 'GET', path: '/masters/{sport}-{year}', config: masters.get});
-
-  // Masters events
-  plugin.route({method: 'GET', path: '/masters/events', config: masters.events(masterChannel)});
-  sub.addChannel('masters', (event) => masterChannel.write({event: `masters-${event}`}));
 
   return done();
 };
 
 exports.register.attributes = {
-  name: `x-${packageInfo.name}-api`,
+  name: `x-${packageInfo.name}-routes`,
   version: packageInfo.version
 };
