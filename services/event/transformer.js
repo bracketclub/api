450a5c542e30991f95916b7cec2b51326244acd9
Luke Karrys <lukekarrys@gmail.com>
2016-12-09 15:53:15 -0700

    reimplement susie locally to get heartbeats

---
+++ services/event/transformer.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const Stream = require('stream');
+
+const END_LINE = '\r\n';
+
+class Transform extends Stream.Transform {
+  constructor(options = {}, objectMode) {
+    super({objectMode});
+
+    this.event = options.event || null;
+
+    let counter = 1;
+    this.generateId = options.generateId || (() => counter++);
+  }
+
+  _transform(chunk, encoding, callback) {
+    if (typeof chunk === 'string' && chunk[0] === ':') {
+      this.push(chunk + END_LINE + END_LINE);
+      return callback();
+    }
+
+    const chunkEvent = (typeof chunk === 'object' ? chunk.event : null) || this.event;
+
+    const event = {
+      id: this.generateId(chunk),
+      data: chunk
+    };
+
+    if (chunkEvent) event.event = chunkEvent;
+
+    this.push(this.stringify(event));
+    return callback();
+  }
+
+  _flush(callback) {
+    this.push(this.stringify({event: 'end', data: ''}));
+    callback();
+  }
+
+  stringify(event) {
+    return Object.keys(event).reduce((memo, key) => {
+      let val = event[key];
+      if (val instanceof Buffer) val = val.toString();
+      if (typeof val === 'object') val = JSON.stringify(val);
+      return `${memo}${key}: ${val}${END_LINE}`;
+    }, '') + END_LINE;
+  }
+}
+
+module.exports = Transform;
--- services/sse/index.js
+++ services/sse/index.js
@@ -4,6 +4,7 @@ const util = require('util');
 const PassThrough = require('stream').PassThrough;
 const PGPubsub = require('pg-pubsub');
 const config = require('getconfig');
+const ms = require('ms');
 
 const packageInfo = require('../../package');
 
@@ -15,12 +16,12 @@ exports.register = (server, options, done) => {
     log: (...args) => server.log([LOG_TAG, 'pgpubsub'], util.format(...args))
   });
 
-  const createSSERoute = (name) => {
+  const createSSERoute = (name, write) => {
     const stream = new PassThrough({objectMode: true});
 
     sub.addChannel(name, (id) => {
       server.log([LOG_TAG, name], id);
-      stream.write({id});
+      stream.write(write(id));
     });
 
     server.route({
@@ -29,14 +30,19 @@ exports.register = (server, options, done) => {
       config: {
         description: `Get ${name} events stream`,
         tags: [LOG_TAG, name],
-        handler: (request, reply) => reply.event(stream, null, {event: name})
+        handler: (request, reply) => reply.event(stream)
       }
     });
+
+    // https://github.com/zeit/now-cli/issues/20
+    // When deployed to now.sh it seems to close streams after 1 or 2 minutes
+    // so this will keep those alive
+    setInterval(() => stream.write(':heartbeat'), ms('30s'));
   };
 
-  createSSERoute('users');
-  createSSERoute('entries');
-  createSSERoute('masters');
+  createSSERoute('users', (id) => ({id, event: 'users'}));
+  createSSERoute('entries', (event) => ({event}));
+  createSSERoute('masters', (event) => ({event}));
 
   return done();
 };
