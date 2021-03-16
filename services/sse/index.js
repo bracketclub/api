1905945d4a06b53e8c1fc3cca6b91f224b8daedf
Luke Karrys <lukekarrys@gmail.com>
2021-03-15 21:33:02 -0700

    redo sse so its all in the handler

--- services/sse/index.js
+++ services/sse/index.js
@@ -8,7 +8,7 @@ const _ = require("lodash");
 const packageInfo = require("../../package");
 const postgres = require("../../lib/postgres-config");
 
-const WRITE_WAIT = 250;
+const WRITE_WAIT = 1000;
 const LOG_TAG = "sse";
 
 exports.register = (server, options, done) => {
@@ -17,21 +17,7 @@ exports.register = (server, options, done) => {
     log: (...args) => server.log([LOG_TAG, "pgpubsub"], util.format(...args)),
   });
 
-  const createSSERoute = (route, write, { debounce = true } = {}) => {
-    // Create a stream which will be the reply and be written to by pg pubsub
-    const eventStream = new PassThrough({ objectMode: true });
-
-    // Log and write to the stream
-    const writeEvent = _[debounce ? "debounce" : "identity"]((id, name) => {
-      const data = write(id);
-      server.log([LOG_TAG, name], data);
-      eventStream.write(data);
-    }, WRITE_WAIT);
-
-    // Add a pubsub channel for the namespace. This will listen for all NOTIFY
-    // queries on that table
-    sub.addChannel(route, (id) => writeEvent(id, route));
-
+  const createSSERoute = (route, write) => {
     server.route({
       method: "GET",
       path: `/${route}/events`,
@@ -39,23 +25,42 @@ exports.register = (server, options, done) => {
         description: `Get ${route} events stream`,
         tags: ["api", LOG_TAG, route],
         handler: (request, reply) => {
+          // Create a stream which will be the reply and be written to by pg pubsub
+          const eventStream = new PassThrough({ objectMode: true });
+
+          // Log and write to the stream
+          const writeEvent = _.debounce((id, name) => {
+            const data = write(id);
+            server.log([LOG_TAG, name], data);
+            eventStream.write(data);
+          }, WRITE_WAIT);
+
+          // Add a pubsub channel for the namespace. This will listen for all NOTIFY
+          // queries on that table
+          const handler = (id) => writeEvent(id, route);
+          sub.addChannel(route, handler);
+
+          // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
+          // Heroku has a 55s idle connection timeout
+          const interval = setInterval(
+            () => eventStream.write({ event: "keepalive", time: Date.now() }),
+            ms("50s")
+          );
+
+          request.once("disconnect", () => {
+            clearInterval(interval);
+            sub.removeChannel(route, handler);
+          });
+
           return reply.event(eventStream);
         },
       },
     });
-
-    // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
-    // Heroku has a 55s idle connection timeout
-    setInterval(() => {
-      eventStream.write({ event: "keepalive", time: Date.now() });
-    }, ms("50s"));
   };
 
   // Don't debounce stream writes for users since individual users wont
   // ever update that frequently.
-  createSSERoute("users", (id) => ({ id, event: "users" }), {
-    debounce: false,
-  });
+  createSSERoute("users", (id) => ({ id, event: "users" }));
   createSSERoute("entries", (event) => ({ event }));
   createSSERoute("masters", (event) => ({ event }));
 
