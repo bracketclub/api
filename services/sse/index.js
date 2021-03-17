a85d3c4cdc7bd9b5a156d57befd0b98b4ab306c5
Luke Karrys <lukekarrys@gmail.com>
2021-03-16 23:09:25 -0700

    redo sse to be a single route

--- services/sse/index.js
+++ services/sse/index.js
@@ -17,55 +17,67 @@ exports.register = (server, options, done) => {
     log: (...args) => server.log([LOG_TAG, "pgpubsub"], util.format(...args)),
   });
 
-  const createSSERoute = (route, write) => {
-    server.route({
-      method: "GET",
-      path: `/${route}/events`,
-      config: {
-        description: `Get ${route} events stream`,
-        tags: [LOG_TAG, route],
-        handler: (request, reply) => {
-          // Create a stream which will be the reply and be written to by pg pubsub
-          const eventStream = new PassThrough({ objectMode: true });
+  server.route({
+    method: "GET",
+    path: `/event-stream`,
+    config: {
+      description: `Get event stream`,
+      tags: [LOG_TAG],
+      handler: (request, reply) => {
+        // Create a stream which will be the reply and be written to by pg pubsub
+        const eventStream = new PassThrough({ objectMode: true });
 
-          const keepalive = () =>
-            eventStream.write({
-              event: "keepalive",
-              time: new Date().toJSON(),
-            });
-
-          // Add a pubsub channel for the namespace. This will listen for all NOTIFY
-          // queries on that table
-          const handler = _.debounce((id) => {
-            const data = write(id);
-            server.log([LOG_TAG, route], data);
+        // Add a pubsub channel for the namespace. This will listen for all NOTIFY
+        // queries on that table
+        const createHandler = (fn) =>
+          _.debounce((id) => {
+            const data = fn(id);
+            request.log([LOG_TAG, data.event], data);
             eventStream.write(data);
           }, WRITE_WAIT);
 
-          sub.addChannel(route, handler);
+        const mastersHandler = createHandler((sportYear) => ({
+          event: "masters",
+          id: sportYear,
+        }));
 
-          // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
-          // Heroku has a 55s idle connection timeout. But in practice it is 30s
-          const keepAliveInterval = setInterval(keepalive, ms("20s"));
-          const initialTimeout = setTimeout(keepalive, ms("2s"));
+        const entriesHandler = createHandler((sportYear) => ({
+          event: "entries",
+          id: sportYear,
+        }));
 
-          request.once("disconnect", () => {
-            clearInterval(keepAliveInterval);
-            clearTimeout(initialTimeout);
-            sub.removeChannel(route, handler);
+        const usersHandler = createHandler((userId) => ({
+          event: "users",
+          id: userId,
+        }));
+
+        sub.addChannel("masters", mastersHandler);
+        sub.addChannel("entries", entriesHandler);
+        sub.addChannel("users", usersHandler);
+
+        const keepalive = () =>
+          eventStream.write({
+            event: "keepalive",
+            time: new Date().toJSON(),
           });
 
-          return reply.event(eventStream);
-        },
-      },
-    });
-  };
+        // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
+        // Heroku has a 55s idle connection timeout. But in practice it is 30s
+        const keepAliveInterval = setInterval(keepalive, ms("20s"));
+        const initialTimeout = setTimeout(keepalive, ms("2s"));
 
-  // Don't debounce stream writes for users since individual users wont
-  // ever update that frequently.
-  createSSERoute("users", (id) => ({ id, event: "users" }));
-  createSSERoute("entries", (event) => ({ event }));
-  createSSERoute("masters", (event) => ({ event }));
+        request.once("disconnect", () => {
+          clearInterval(keepAliveInterval);
+          clearTimeout(initialTimeout);
+          sub.removeChannel("masters", mastersHandler);
+          sub.removeChannel("entries", entriesHandler);
+          sub.removeChannel("users", usersHandler);
+        });
+
+        return reply.event(eventStream);
+      },
+    },
+  });
 
   return done();
 };
